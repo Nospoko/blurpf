37aaca52f9d156aa2ab298d1c017a05927652c8c
Tomasz Roszczynialski <roszcz@gmail.com>
2016-05-24 11:40:56 +0200

    Add OpenCV colormaps support

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import cv2 as cv
 import numpy as np
-import scipy.io.wavfile as io
+from utils import signal as us
 from matplotlib import pyplot as plt
+
+def funky_image(XX, YY, tick):
+    """ Generate some funk """
+    Z = np.sin(13.*(XX**3 + YY**2) + np.sin(tick/80.0)) *\
+        np.cos(tick/30. + 3 * np.arctan2(XX**2, YY**3)) *\
+        np.cos(tick/44. + 7 * np.arctan2(XX, YY))
+
+    Z -= Z.min()
+    Z /= Z.max()
+    Z *= 256.
+
+    # OpenCV likes uint8
+    return np.uint8(Z)
 
 def main():
     """ blurp """
-    filepath = 'wavs/PIANO_23_04.wav'
-    fs, x = io.read(filepath)
+    x = np.linspace(-1, 1, 401)
+    y = np.linspace(-1, 1, 301)
+    XX, YY = np.meshgrid(x, y)
 
-    plt.plot(x[0:1000])
-    plt.show()
+    for tick in range(1000):
+        print tick
+        ZZ = funky_image(XX, YY, 8*tick)
+        filename = 'imgs/{}.png'.format(1e7 + tick)
+        img = cv.applyColorMap(ZZ, cv.COLORMAP_AUTUMN)
+        cv.imwrite(filename, img)
 
 if __name__ == '__main__':
     main()
---
+++ utils/signal.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy.io.wavfile as io
+
+def read_wav():
+    fs, x = io.read(filepath)
+
+    return fs, x
+
+def get_stereo(filepath = 'wavs/PIANO_23_04.wav'):
+    """ Read into 2 np.arrays (no mono) """
+    fs, x = io.read(filepath)
+
+    return x[:, 0], x[:, 1]
+
