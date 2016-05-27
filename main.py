533ba941623566efa7c608bf1c34a8d8fa9b4889
Tomasz Roszczynialski <roszcz@gmail.com>
2016-05-27 08:13:09 +0200

    blurp commit with 6 symmetric walkers

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ def norm(this):
 
 def funky_image(XX, YY, tick):
     """ Generate some funk """
-    phi = 10*np.pi * 2 * tick/2500.0
+    phi = 4*np.pi * 2 * tick/2500.0
     the = 8*np.pi * 2 * tick/1250.0
 
-    r_shift = 8 + 0.0 * np.cos(the)
+    r_shift = 16 + 3.0 * np.cos(the)
 
     ax_shift = r_shift * np.cos(phi)
     ay_shift = r_shift * np.sin(phi)
 
-    a_hahn = yo.Hahn(k = 0.8, r = 33, m = 2)
+    a_hahn = yo.Hahn(k = 0.5, m = 12)
     a_hahn.set_x_shift(ax_shift)
     a_hahn.set_y_shift(ay_shift)
     Za = a_hahn.get(XX, YY, tick)
@@ -29,7 +29,7 @@ def funky_image(XX, YY, tick):
     bx_shift = r_shift * np.cos(phi - np.pi * 2./3)
     by_shift = r_shift * np.sin(phi - np.pi * 2./3)
 
-    b_hahn = yo.Hahn(k = 0.8, m = 5)
+    b_hahn = yo.Hahn(k = 0.5, m = 12)
     b_hahn.set_x_shift(bx_shift)
     b_hahn.set_y_shift(by_shift)
     Zb = b_hahn.get(XX, YY, tick)
@@ -37,12 +37,34 @@ def funky_image(XX, YY, tick):
     cx_shift = r_shift * np.cos(phi + np.pi * 2./3)
     cy_shift = r_shift * np.sin(phi + np.pi * 2./3)
 
-    c_hahn = yo.Hahn(k = 0.8, m = 12)
+    c_hahn = yo.Hahn(k = 0.5, m = 12)
     c_hahn.set_x_shift(cx_shift)
     c_hahn.set_y_shift(cy_shift)
     Zc = c_hahn.get(XX, YY, tick)
 
-    Z = norm(Za) + norm(Zb) + norm(Zc)
+    dx_shift = r_shift * np.cos(phi + np.pi)
+    dy_shift = r_shift * np.sin(phi + np.pi)
+
+    d_hahn = yo.Hahn(k = 0.5, m = 12)
+    d_hahn.set_x_shift(dx_shift)
+    d_hahn.set_y_shift(dy_shift)
+    Zd = d_hahn.get(XX, YY, tick)
+
+    ex_shift = r_shift * np.cos(phi + np.pi/3)
+    ey_shift = r_shift * np.sin(phi + np.pi/3)
+
+    d_hahn.set_x_shift(ex_shift)
+    d_hahn.set_y_shift(ey_shift)
+    Ze = d_hahn.get(XX, YY, tick)
+
+    fx_shift = r_shift * np.cos(phi - np.pi/3)
+    fy_shift = r_shift * np.sin(phi - np.pi/3)
+
+    d_hahn.set_x_shift(fx_shift)
+    d_hahn.set_y_shift(fy_shift)
+    Zf = d_hahn.get(XX, YY, tick)
+
+    Z = norm(Za) + norm(Zb) + norm(Zc) + norm(Zd) + norm(Ze) + norm(Zf)
 
     # Z = 3 * norm(Za) + \
     #         norm(Zb) * 1.5 * (1 + 0.5 * np.cos(phi - np.pi/2)) + \
@@ -51,7 +73,7 @@ def funky_image(XX, YY, tick):
     # le normalizatione
     Z -= Z.min()
     Z /= Z.max()
-    Z *= 256.
+    Z *= 140 + 64 * np.cos(phi)
 
     # OpenCV likes uint8
     return np.uint8(Z)
@@ -63,18 +85,18 @@ def make_single(tick):
         x_res = 1920
         y_res = 1080
     else:
-        x_res = 540
-        y_res = 540
+        x_res = 590
+        y_res = 440
 
-    xspan = 18.5 + 4*np.cos(8.0 * tick/100)
-    yspan = 18.5 + 4*np.cos(8.0 * tick/100)
+    xspan = 8.5 + 4*np.cos(2.0 * tick/100)
+    yspan = 8.5 + 4*np.cos(2.0 * tick/100)
     x = np.linspace(-xspan, xspan, x_res)
     y = np.linspace(-yspan, yspan, y_res)
     XX, YY = np.meshgrid(x, y)
 
-    ZZ = funky_image(XX, YY, 8*tick)
+    ZZ = funky_image(XX, YY, tick)
     filename = 'imgs/{}.png'.format(int(1e7 + tick))
-    img = cv.applyColorMap(ZZ, cv.COLORMAP_OCEAN)
+    img = cv.applyColorMap(ZZ, cv.COLORMAP_RAINBOW)
     cv.imwrite(filename, img)
 
 def main():
@@ -89,7 +111,7 @@ def main():
              [59, 42, 2, 67],
              [64, 44, 4, 69]]
 
-    tick_range = range(100)
+    tick_range = range(1000)
     pool = mp.Pool(processes = mp.cpu_count())
     pool.map(make_single, tick_range)
 
