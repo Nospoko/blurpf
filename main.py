50955540f974c215e865fbdb9fba1051c8fd197b
Tomasz Roszczynialski <roszcz@gmail.com>
2016-07-14 14:26:05 +0200

    Minor cleanup and various soliton tryouts

--- main.py
+++ main.py
@@ -21,10 +21,43 @@ def soliton(x, where):
 
     return out
 
+def make_box():
+    """ Needed as axes range setter, due to tha lack of such """
+    # Define helpers
+    res = 20
+    width = 5
+    eye = np.ones(res)
+
+    # Draw the 3d box to solve scaling problems
+    box_span = np.linspace(-width, width, res)
+
+    # X-dir
+    mlab.plot3d(box_span, 0 * eye, - width * eye, tube_radius=0.00001)
+    mlab.plot3d(box_span, 0 * eye, + width * eye, tube_radius=0.00001)
+    # Y-dir
+    mlab.plot3d(- width * eye, box_span, 0 * eye, tube_radius=0.00001)
+    mlab.plot3d(+ width * eye, box_span, 0 * eye, tube_radius=0.00001)
+    # Z-dir
+    mlab.plot3d(- width * eye, 0 * eye, box_span, tube_radius=0.00001)
+    mlab.plot3d(+ width * eye, 0 * eye, box_span, tube_radius=0.00001)
+
+def set_camera_position(args):
+    """ Filming angles, pro-shot illusions """
+    # Extract values
+    tick = args['tick']
+    tick = 119
+    phi = args['phi'] * 5
+
+    # Camera settings
+    # Horizontal angle [0 : 360]
+    azimuth = 69 + 3 * tick % 360
+    # Zenith angle [0-180]
+    elevation = 90 + 0 * np.sin(phi)
+    mlab.view(azimuth, elevation, 16.0)
+
 def make_single(args):
     """ Mayavi tryouts """
     res = 500
-    eye = np.ones(res)
 
     # De-serialize arguments
     phi = args['phi'] * 5
@@ -33,64 +66,63 @@ def make_single(args):
     # Make clear figure
     mlab.clf()
 
-    # Camera angle?
-    # Zenith angle [0-180]
-    azimuth = 69
-    elevation = 90 + 0 * np.sin(phi)
-    mlab.view(azimuth, elevation, 16.0)
-
-    # Draw the 3d box to solve scaling problems
-    box_span = np.linspace(-5, 5, res)
-    # X-dir
-    mlab.plot3d(box_span, 0 * eye, - 5 * eye, tube_radius=0.00001)
-    mlab.plot3d(box_span, 0 * eye, + 5 * eye, tube_radius=0.00001)
-    # Y-dir
-    mlab.plot3d(- 5 * eye, box_span, 0 * eye, tube_radius=0.00001)
-    mlab.plot3d(+ 5 * eye, box_span, 0 * eye, tube_radius=0.00001)
-    # Z-dir
-    mlab.plot3d(- 5 * eye, 0 * eye, box_span, tube_radius=0.00001)
-    mlab.plot3d(+ 5 * eye, 0 * eye, box_span, tube_radius=0.00001)
+    # Adjust camera position
+    set_camera_position(args)
+    # Control visual span
+    make_box()
 
     # Create angular solitons
     t = np.linspace(0, 20, res)
+    x = np.linspace(-5, 5, res)
+
     where_a = 10 + 4 * np.cos(5*phi)
     theta_a = soliton(t, where_a)
+    # TODO add rotations of given lines and we're golden
+    y_red = 1.2*np.cos(theta_a)-1 + 0.09 * np.sin(7*x - 3*phi)
+    z_red = 1.2*np.sin(theta_a) + 0.01 * np.cos(5*x + 5*phi)
 
     where_b = 10 - 4 * np.cos(5*phi)
-    theta_b = soliton(t, where_b) + 4*np.pi/3
+    theta_b = soliton(t, where_b)
+
+    y_blue = 1.4*np.cos(theta_b)-1.2 + 0.03 * np.cos(6*x + 4*phi)
+    z_blue = np.sin(theta_b) + 0.02 * np.sin(6*x + 4*phi)
+    y_blue, z_blue = rotate_x(y_blue, z_blue, 2*np.pi/5)
 
     where_c = 8 + 7 * np.sin(6*phi)
-    theta_c = soliton(t, where_c) + 2*np.pi/3
+    theta_c = soliton(t, where_c) + 0*np.pi/3
 
-    x = np.linspace(-5, 5, res)
-    # TODO add rotations of given lines and we're golden
-    y_red = np.cos(theta_a) + 0.09 * np.sin(7*x - 3*phi)
-    z_red = np.sin(theta_a) + 0.1 * np.cos(5*x + 5*phi)
+    y_green = 1.5*np.cos(theta_c)-1.3 + 0.02 * np.sin(8*x + 3*phi)
+    z_green = 1.5*np.sin(theta_c) + 0.01 * np.cos(2*x - 4*phi)
+    y_green, z_green = rotate_x(y_green, z_green, 4*np.pi/5)
 
-    y_blue = np.cos(theta_b) + 0.13 * np.cos(6*x + 4*phi)
-    z_blue = np.sin(theta_b) + 0.12 * np.sin(6*x + 4*phi)
-    y_blue, z_blue = rotate_x(y_blue, z_blue, 4*np.pi/3)
+    where_d = 12 + 6 * np.sin(8*phi)
+    theta_d = soliton(t, where_d) + 0*np.pi/3
 
-    y_green = np.cos(theta_c) + 0.12 * np.sin(8*x + 3*phi)
-    z_green = np.sin(theta_c) + 0.11 * np.cos(2*x - 4*phi)
-    y_green, z_green = rotate_x(y_green, z_green, 2*np.pi/3)
+    y_pink = 1.3*np.cos(theta_d)-1.1 + 0.02 * np.sin(8*x + 3*phi)
+    z_pink = 1.3*np.sin(theta_d) + 0.04 * np.cos(2*x - 4*phi)
+    y_pink, z_pink = rotate_x(y_pink, z_pink, 6*np.pi/5)
 
     s_a = np.gradient(theta_a)
     s_b = np.gradient(theta_b)
     s_c = np.gradient(theta_c)
+    s_d = np.gradient(theta_d)
 
     # One
     mlab.plot3d(x, y_green, z_green, s_c,
                 colormap='Greens',
-                tube_radius=0.02)
+                tube_radius=0.03)
     # Two
     mlab.plot3d(x, y_red, z_red, s_a,
                 colormap='Reds',
-                tube_radius=0.02)
+                tube_radius=0.034)
     # Three
     mlab.plot3d(x, y_blue, z_blue, s_b,
                 colormap='Blues',
-                tube_radius=0.02)
+                tube_radius=0.029)
+    # 4 
+    mlab.plot3d(x, y_pink, z_pink, s_d,
+                colormap='gist_rainbow',
+                tube_radius=0.039)
 
     savepath = 'imgs/frame_{}.png'.format(1000000 + tick)
     mlab.savefig(savepath)
@@ -108,10 +140,10 @@ def main():
         scores = pickle.load(fin)
 
     # Generate movie factors
-    args = ua.score2args(scores)[:160]
+    args = ua.score2args(scores)[:]
 
     # Not parallel
-    # FIXME how to make hd aspect-ratio?
+    # FIXME how to make full-hd
     mlab.figure(fgcolor=(1, 1, 1), bgcolor=(0, 0, 0), size=(700, 700))
 
     for arg in args:
